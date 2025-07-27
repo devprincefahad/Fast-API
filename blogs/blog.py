6a35fb8813a3277720d7ce0a19ab2ac7bf2f571d
devprincefahad <1pfchouhan@gmail.com>
2025-07-27 21:37:03 +0530

    relationship between user and blogs

--- blogs/blog.py
+++ blogs/blog.py
@@ -16,15 +16,15 @@ def get_db():
     finally:
         db.close() 
            
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog,  db: Session = Depends(get_db)): 
-    new_blog = models.Blog(title= request.title, body = request.body)
+    new_blog = models.Blog(title= request.title, body = request.body, user_id = 1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}')
+@app.delete('/blog/{id}', tags=['blogs'])
 def delete(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -34,12 +34,12 @@ def delete(id, db: Session = Depends(get_db)):
     db.commit()
     return {'Deleted successfully'}
 
-@app.get('/blog',response_model = List[schemas.ShowBlog])
+@app.get('/blog',response_model = List[schemas.ShowBlog], tags=['blogs'])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     # db.query(models.Blog).filter(models.Blog.id == id).update(request)
     blog = db.query(models.Blog).filter(models.Blog.id == id)
@@ -53,7 +53,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return 'update'
 
-@app.get('/blog/{id}', status_code=200, response_model = schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model = schemas.ShowBlog, tags=['blogs'])
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -63,7 +63,7 @@ def show(id, response: Response, db: Session = Depends(get_db)):
         #  return {'detail': f'Blog with {id} is not available.'}
     return blog
 
-@app.post('/user', response_model=schemas.ShowUser)
+@app.post('/user', response_model=schemas.ShowUser, tags=['users'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     # hashed_password = pwd_cxt.hash(request.password)
     new_user = models.User(name = request.name, 
@@ -74,7 +74,7 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get('/user/{id}', response_model = schemas.ShowUser)
+@app.get('/user/{id}', response_model = schemas.ShowUser, tags=['users'])
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
--- blogs/schemas.py
+++ blogs/schemas.py
@@ -1,12 +1,11 @@
 from pydantic import BaseModel
+from typing import List
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
+class Blog(BlogBase):
     class Config():
         orm_mode = True
 
@@ -18,5 +17,13 @@ class User(BaseModel):
 class ShowUser(BaseModel):
     name: str
     email: str
+    blogs: List[Blog] = []
+
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BlogBase):
+    creator: ShowUser
+
     class Config():
         orm_mode = True
