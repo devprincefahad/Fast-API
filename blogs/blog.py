cbe9a9411cd4f881d688c4df13dbbcbf65a36fcf
devprincefahad <1pfchouhan@gmail.com>
2025-07-27 15:06:32 +0530

    User Model and password hashing

--- blogs/blog.py
+++ blogs/blog.py
@@ -3,6 +3,7 @@ from blogs import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -52,12 +53,31 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return 'update'
 
-@app.get('/blog/{id}', status_code=201, response_model = schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model = schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-                            detail = "Blog with {id} is not available.")
+                            detail = f"Blog with {id} is not available.")
         #  response.status_code = status.HTTP_404_NOT_FOUND
         #  return {'detail': f'Blog with {id} is not available.'}
     return blog
+
+@app.post('/user', response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    # hashed_password = pwd_cxt.hash(request.password)
+    new_user = models.User(name = request.name, 
+                           email = request.email, 
+                           password = Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/user/{id}', response_model = schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = f"User with {id} is not available.")
+    return user
--- blogs/schemas.py
+++ blogs/schemas.py
@@ -9,3 +9,14 @@ class ShowBlog(BaseModel):
     body: str
     class Config():
         orm_mode = True
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    class Config():
+        orm_mode = True
